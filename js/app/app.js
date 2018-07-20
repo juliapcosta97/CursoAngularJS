810bf317b443d5d4d76f47cc58c4c47712673445
Julia Pereira <juliapcosta97@gmail.com>
2018-07-20 16:51:17 -0300

    Trabalhando com rotas

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,15 @@
-angular.module("meuModulo",[])
+angular.module("meuModulo",['ngRoute'])
+.config(function($routeProvider){
+	$routeProvider
+	.when("/home",{
+		templateUrl:"templates/home.html",
+		controller:"indexController"
+	})
+	.when("/contato",{
+		templateUrl:"templates/contato.html",
+		controller:"contatoController"
+	});
+
+	//definindo uma rota padrão
+	$routeProvider.otherwise({redirectTo:"/home"});
+})
